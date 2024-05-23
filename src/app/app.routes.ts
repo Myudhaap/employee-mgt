3f833f1a4f67bceb30977d7cf5633de7fdcfa247
yudhaPratama <muhyudhapratama55@gmail.com>
2024-05-23 13:14:28 +0700

    [IMPROVE] install starter depedency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutMainComponent } from './shared/components/layout/layout-main/layout-main.component';
+import { LayoutAuthComponent } from './shared/components/layout/layout-auth/layout-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutMainComponent,
+    },
+    {
+        path: "auth",
+        component: LayoutAuthComponent
+    }
+];
---
+++ src/app/shared/components/layout/layout-auth/layout-auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-auth.component.html',
+  styleUrl: './layout-auth.component.scss'
+})
+export class LayoutAuthComponent {
+
+}
